e3080b2dcacfe1981a180c3723f95da353abd646
lat-sc <41201532+lat-sc@users.noreply.github.com>
2023-02-11 21:04:49 -0500

    Update streamlit-app.py
    filter table

--- streamlit-app.py
+++ streamlit-app.py
@@ -11,5 +11,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit');
 
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries']);
-streamlit.dataframe(my_fruit_list)
+selected_fruits = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries']);
+
+show_fruits = my_fruit_list.loc(selected_fruits)
+streamlit.dataframe(show_fruits)
